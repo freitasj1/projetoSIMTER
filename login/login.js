d3ef6ed7a2a57bf2f71fd508c83c16276341e5ae
freitasj1 <joaopedromacielfreitas@outlook.com>
2024-04-16 18:21:44 -0300

    add page de login

---
+++ login/login.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("login-form");
+    const errorMessage = document.getElementById("error-message");
+
+   
+    const users = [
+        { username: "admin", password: "admin" },
+        { username: "Pinguim", password: "PoloNorte" },
+        { username: "FrangoFrito", password: "Senha" },
+        { username: "ProjetoHERO", password: "projete" },
+        { username: "BobEsponja", password: "LulaMolusco" },
+
+
+    
+    ];
+
+    loginForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+       
+        const user = users.find(user => user.username === username && user.password === password);
+
+        if (user) {
+            errorMessage.textContent = "Login bem-sucedido!";
+            location.href = 'section-one.html';
+        } else {
+            errorMessage.textContent = "Credenciais inválidas";
+        }
+    });
+});
+
