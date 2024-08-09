47e0a8400c848df566809964a01ec560a8524a31
freitasj1 <joaopedromacielfreitas@outlook.com>
2024-08-08 23:58:14 -0300

    remoção do react do projeto Haverá uma troca de React para Bootstrap, devido a facilidade de uso

--- vite-project/src/index.js
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css'; // Adicione estilos conforme necessário
-
-ReactDOM.render(<App />, document.getElementById('root'));
